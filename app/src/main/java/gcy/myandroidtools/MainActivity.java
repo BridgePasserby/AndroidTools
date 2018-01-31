19a07b893b081af244b90a999b9a7ac3810ec13a
youthCN <651323312@qq.com>
2018-01-31 14:49:41 +0800

    title:实现一些安卓开发中常用的工具 cause: solution： effect: reviewer:

---
+++ app/src/main/java/gcy/myandroidtools/MainActivity.java
@@ -0,0 +1,15 @@
+package gcy.myandroidtools;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import gcy.androidtools.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/gcy/myandroidtools/common/util/BaseApp.java
@@ -0,0 +1,42 @@
+package gcy.myandroidtools.common.util;
+
+/**
+ * Copyright (c) 2018, 北京视达科科技有限责任公司 All rights reserved.
+ * author：chongyang.gao
+ * date：2018/1/31
+ * description：
+ */
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Handler;
+
+public class BaseApp extends Application {
+    private static BaseApp application;
+    private static int mainTid;
+    private static Handler handler;
+
+    @Override
+//  在主线程运行的
+    public void onCreate() {
+        super.onCreate();
+        application = this;
+        mainTid = android.os.Process.myTid();
+        handler = new Handler();
+    }
+
+    public static Context getApplication() {
+        return application;
+    }
+
+    public static int getMainTid() {
+        return mainTid;
+    }
+
+    public static Handler getHandler() {
+        return handler;
+    }
+
+
+}
+
