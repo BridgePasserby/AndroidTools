fb8baf6fe87c1a2829607e3e22defa452d835e26
chongyang.gao <651323312@qq.com>
2018-12-17 14:30:54 +0800

    title:提交两个测试的Activity cause: solution： effect: reviewer:

---
+++ app/src/main/java/gcy/androidtools/SecondActivity.java
@@ -0,0 +1,32 @@
+package gcy.androidtools;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = "SecondActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_main);
+        Log.i(TAG, "onCreate: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+}
